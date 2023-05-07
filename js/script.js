90bafa373a272c69b4127253748f529d12e51538
azharrahad31 <azherudinrahad@gmail.com>
2023-05-07 17:59:06 +0600

    recive all html tag or id on js file

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const postBtn=document.querySelector('#postBtn');
+const inputField=document.querySelector('#inputField');
+const error=document.querySelector('h1');
+const ul=document.querySelector('ul');
+console.log(ul)
