e64deaa4a843404551a148f7c24bedf216a1755c
azharrahad31 <azherudinrahad@gmail.com>
2023-05-07 18:16:52 +0600

    use create a post click a post btn

--- js/script.js
+++ js/script.js
@@ -2,4 +2,21 @@ const postBtn=document.querySelector('#postBtn');
 const inputField=document.querySelector('#inputField');
 const error=document.querySelector('h1');
 const ul=document.querySelector('ul');
-console.log(ul)
+const arr=[];
+console.log(ul);
+postBtn.addEventListener('click',function(){
+    if(!Boolean(inputField.value)){
+        error.innerText='Type post on input filed'
+    }
+    else{
+        error.innerText="";
+        arr.push(inputField.value);
+        ul.innerHTML=""
+        arr.map((item,index)=>{
+            ul.innerHTML +=`<li data-id=${index}>${item} <button class="edit">Edit</button> <button class="delete">Delete</button></li>`;
+        })
+
+    }
+
+
+})
