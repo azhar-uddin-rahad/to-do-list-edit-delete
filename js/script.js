27093ac4a3f8bc9a122ad177fe01ffaa344dd50a
azharrahad31 <azherudinrahad@gmail.com>
2023-05-07 20:30:45 +0600

    update btn work

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ const postBtn=document.querySelector('#postBtn');
 const inputField=document.querySelector('#inputField');
 const error=document.querySelector('h1');
 const ul=document.querySelector('ul');
+const update=document.querySelector('#updateBtn')
 const arr=[];
+let updatedId=''
 console.log(ul);
 postBtn.addEventListener('click',function(){
     if(!Boolean(inputField.value)){
@@ -11,33 +13,46 @@ postBtn.addEventListener('click',function(){
     else{
         error.innerText="";
         arr.push(inputField.value);
-        function common(){
-            ul.innerHTML="";
-        arr.map((item,index)=>{
-            ul.innerHTML +=`<li data-id=${index}>${item} <button class="edit">Edit</button> <button class="delete">Delete</button></li>`;
-            const deleteBtn=document.querySelectorAll('.delete');
-            const deleteBtnArr=Array.from(deleteBtn);
-            deleteBtnArr.map((item,ix)=>{
-               item.addEventListener('click',function(){
-                arr.splice(ix,1);
-                common();
-                console.log(arr);
-               })
-            })
-
-            const editBtn=document.querySelectorAll('#edit');
-            const editBtnArr=Array.from(editBtn);
-            editBtnArr.map((item2,idx)=>{
-                item2.addEventListener('click',function(){
-                    console.log(item2);
-                })
-            })
-
- })
-        }
+        
         common();
 
     }
 
+
+})
+function common(){
+    ul.innerHTML="";
+    arr.map((item,index)=>{
+    ul.innerHTML +=`<li data-id=${index}>${item} <button class="edit">Edit</button> <button class="delete">Delete</button></li>`;
+    const deleteBtn=document.querySelectorAll('.delete');
+    const deleteBtnArr=Array.from(deleteBtn);
+    deleteBtnArr.map((item,ix)=>{
+       item.addEventListener('click',function(){
+        arr.splice(ix,1);
+        common();
+        console.log(arr);
+       })
+    })
+
+    const editBtn=document.querySelectorAll('.edit');
+    const editBtnArr=Array.from(editBtn);
+    editBtnArr.map((item2,idx)=>{
+        item2.addEventListener('click',function(){
+            inputField.value = arr[idx];
+            inputField.focus();
+            updatedId=idx;
+            update.style.display='block';
+            postBtn.style.display='none';
+
+        })
+    })
+
+})
+}
+update.addEventListener('click',function(){
+    arr[updatedId]=inputField.value;
+    common();
+    update.style.display='none';
+    postBtn.style.display='block';
 
 })
