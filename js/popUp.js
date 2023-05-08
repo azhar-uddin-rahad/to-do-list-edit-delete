2e95e75736c8dc235f21bf59a30a2c16153f61ee
azharrahad31 <azherudinrahad@gmail.com>
2023-05-09 00:42:58 +0600

    javascript pop up project end

--- js/popUp.js
+++ js/popUp.js
@@ -4,6 +4,13 @@ document.querySelector('#popUpBtn').addEventListener('click',function(){
     overly.style.display='flex';
 })
 
+overly.addEventListener('click',function(event){
+   /// console.log(event.target.classList[0]);
+    if(event.target.classList[0] == 'overly'){
+        overly.style.display='none';
+    }
+
+})
 document.querySelector('#close').addEventListener('click',function(){
     overly.style.display='none';
 })
