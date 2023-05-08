fc00a44b1b70ece443b93286e790efc882b41071
azharrahad31 <azherudinrahad@gmail.com>
2023-05-09 01:47:54 +0600

    javaScript simple parallax js project end

--- js/parallax.js
+++ js/parallax.js
@@ -1,5 +1,11 @@
+const one =document.querySelector('.img1');
+const two =document.querySelector('.img2');
+console.log(two)
+window.addEventListener('mousemove',function(event){
+    console.log(event.clientX);
+    one.style.left= (event.clientX/5) + 'px';
+    one.style.top= (event.clientY/5) + 'px';
+    two.style.left= (event.clientX/15) + 'px';
+    two.style.top= (event.clientY/15) + 'px';
 
-
-window.addEventListener('mousemove',function(){
-    
 })
