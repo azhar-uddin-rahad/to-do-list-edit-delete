86b605c56561aab11fe26e4d36b91f2d79177968
azharrahad31 <azherudinrahad@gmail.com>
2023-05-09 20:48:21 +0600

    when user view target class then start text like type js

--- js/oneClassAddTypeStar.js
+++ js/oneClassAddTypeStar.js
@@ -4,29 +4,49 @@ console.log(textArr);
 const color=['red','blue','orange','prink','yellow','green','tomato','syne']
 let typingTextArr='';
 
+
+
 textArr.map((item) =>{
+    function isInViewPort(){
+        const viewport=item.getBoundingClientRect();
+        return(
+            viewport.top>=0 && viewport.left>=0 && viewport.bottom<=(window.innerHeight || document.documentElement.clientHeight) &&
+            viewport.right <= (window.innerWidth || document.documentElement.clientWidth)
+        )
+    
+    }
     const typingTextStore=item.innerText;
     console.log(typingTextStore);
     item.innerText ='';
     let count=0;
-    function typeJS(){
-        if(count < typingTextStore.length){
-            item.innerHTML += `<span style="color:${color[count]}">${typingTextStore.charAt(count)}</span>`;
-            count ++;
-           typingTextArr=typingTextStore.split('');  
+ let speed=true;
+  window.addEventListener('scroll',function(){
+    if(isInViewPort() && speed){
+        speed=false;
+        function typeJS(){
+            console.log(isInViewPort());
+            if(count < typingTextStore.length){
+                item.innerHTML += `<span style="color:${color[count]}">${typingTextStore.charAt(count)}</span>`;
+                count ++;
+               typingTextArr=typingTextStore.split('');  
+            }
+             else{
+               
+                typingTextArr.pop();     
+                item.innerHTML = typingTextArr.join("");
+                if(typingTextArr.length === 0){
+                   count=0;
+                } 
+            }
         }
-         else{
-            console.log(typingTextArr)
-            typingTextArr.pop();     
-            item.innerHTML = typingTextArr.join("");
-            if(typingTextArr.length === 0){
-               count=0;
-            } 
-        }
+        setInterval(()=>{
+            typeJS()
+        },item.dataset.speed)
     }
-    setInterval(()=>{
-        typeJS()
-    },item.dataset.speed)
+  })
+
+    
+ 
     })
    
 
